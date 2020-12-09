6672e63e237d0ca73e2c7c015a369416e28509e3
Bahr23 <35177244+Bahr23@users.noreply.github.com>
2020-12-09 19:38:59 +0300

    Add files via upload
    models

--- models.py
+++ models.py
@@ -6,11 +6,11 @@ from pony.orm import *
 
 db = Database()
 
-db.bind(provider='postgres', user='postgres', password='Tiraspol_2003', host='localhost', database='studybot')
+# db.bind(provider='postgres', user='postgres', password='', host='localhost', database='')
 
-# db_url = urlparse(os.environ['DATABASE_URL'])
-# args = re.split('[:@]', db_url.netloc)
-# db.bind(provider=db_url.scheme, user=args[0], password=args[1], host=args[2], port=args[3], database=db_url.path[1:])
+db_url = urlparse(os.environ['DATABASE_URL'])
+args = re.split('[:@]', db_url.netloc)
+db.bind(provider=db_url.scheme, user=args[0], password=args[1], host=args[2], port=args[3], database=db_url.path[1:])
 
 
 class User(db.Entity):
