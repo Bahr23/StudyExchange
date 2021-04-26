d1234307b11206a3174cb25d8d04d1d41fcaf418
Bahr23 <35177244+Bahr23@users.noreply.github.com>
2021-04-26 23:09:50 +0300

    Add files via upload

--- create_chat.py
+++ create_chat.py
@@ -8,16 +8,14 @@ if len(sys.argv) == 2:
     order_id = sys.argv[1]
 else:
     sys.exit(0)
-#
-# order_id = 45
 
 api_id = 5177145
 api_hash = 'b62cd3362a6e7f553887cf0e3bbffaf8'
 
 with TelegramClient('session_name', api_id, api_hash) as client:
     # bot_id = 1531692979
-    # bot_id = 1480198758
-    bot_id = '🤖 Бот StudyX'
+    bot_id = 1480198758
+    # bot_id = '🤖 Бот StudyX'
     result = client(functions.messages.CreateChatRequest(users=[bot_id], title='Заказ #{}'.format(order_id)))
     chat_id = result.chats[0].id
     client(functions.messages.EditChatAdminRequest(chat_id=chat_id, user_id=bot_id, is_admin=True))
